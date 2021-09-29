ef6193f67bea5497a50e5293d25ace10e07d81de
Arti14MRN <91422101+Arti14MRN@users.noreply.github.com>
2021-09-29 20:50:43 +0900

    add Lesson2_1
    разбил на проекты

---
+++ Lesson2/Lesson2_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson2_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ///Проект 1.Запросить у пользователя минимальную и максимальную температуру за сутки и вывести среднесуточную температуру.
+            
+            Console.Write("Введите Минимальную Температуру за сутки:");
+            int mintemp = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите Максимальную Температуру за сутки:");
+            int maxtemp = Convert.ToInt32(Console.ReadLine());
+            double diff = (mintemp + maxtemp);
+            Console.WriteLine($"среднесуточная температура = {diff / 2}");
+        }
+    }
+}
