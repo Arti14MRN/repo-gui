9e54682b78da24e668a5ac279f43e49a300f6bca
Arti14MRN <91422101+Arti14MRN@users.noreply.github.com>
2021-09-29 12:57:29 +0900

    add Lesson1

---
+++ Lesson1/Lesson1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter ur name");
+            var Name = Console.ReadLine();
+            Console.WriteLine($"Hello {Name}!");
+        }
+    }
+}
