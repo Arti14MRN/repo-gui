0ed1f30e53c42cae75560774f88f9fcb5f7b9ecd
Arti14MRN <91422101+Arti14MRN@users.noreply.github.com>
2021-10-13 00:21:56 +0900

    Lesson5
    add Lesson5

---
+++ Lesson5/Lesson5_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace Lesson5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ввести с клавиатуры произвольный набор данных и сохранить его в текстовый файл.
+            Console.WriteLine("введите данные для сохранения в file.txt");
+            var data = Console.ReadLine();
+            File.WriteAllText("file.txt", data);
+        }
+    }
+}
